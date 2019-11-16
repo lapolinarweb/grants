36f492f4c6148855b9d6964d354008e96b8646be
Superern <ernandrewgregorio@gmail.com>
2019-11-17 05:56:29 +0800

    create relationships in applicant model

--- app/Models/Applicant.php
+++ app/Models/Applicant.php
@@ -7,4 +7,36 @@ use Illuminate\Database\Eloquent\Model;
 class Applicant extends Model
 {
     protected $fillable = ['title', 'firstname', 'lastname', 'type_id'];
+
+    public function type() {
+        return $this->belongsTo(Type::class);
+    }
+
+    public function addresses() {
+        return $this->hasMany(Address::class);
+    }
+
+    public function mobiles() {
+        return $this->hasMany(Mobile::class);
+    }
+
+    public function emails() {
+        return $this->hasMany(Email::class);
+    }
+
+    public function passports() {
+        return $this->hasMany(Passport::class);
+    }
+
+    public function publicKeys() {
+        return $this->hasMany(PublicKey::class);
+    }
+
+    public function countries() {
+        return $this->belongsToMany(Country::class);
+    }
+
+    public function statuses() {
+        return $this->belongsToMany(Status::class);
+    }
 }
