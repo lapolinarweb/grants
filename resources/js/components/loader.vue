e5486396b8c80273d03f8b2ae85a56fd7280f8df
Superern <ernandrewgregorio@gmail.com>
2019-11-16 18:57:19 +0800

    add vue-loading-overlay

---
+++ resources/js/components/loader.vue
@@ -0,0 +1,39 @@
+<template>
+    <div ref="vLoader" class="vld-parent"></div>
+</template>
+
+<script>
+    import VueLoading from 'vue-loading-overlay';
+
+    Vue.use(VueLoading);
+
+    export default {
+		name: "loader",
+        components: {
+		    MyLoader
+        },
+        data() {
+		    return {
+		        loader: null
+            }
+        },
+        beforeCreate() {
+            this.loader = this.$loading.show({
+                // Pass props by their camelCased names
+                container: this.$refs.vLoader,
+                loader: 'spinner',
+                opacity: 0.5,
+                zIndex: 999,
+            }, {
+                default: this.$createElement('my-loader')
+            });
+        },
+        mounted() {
+		    // this.loader.hide();
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
