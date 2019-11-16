21521fb6ed2c73fed49aa88347a96d734a98122f
Superern <ernandrewgregorio@gmail.com>
2019-11-17 05:27:23 +0800

    create emails migration

---
+++ database/migrations/2019_11_17_050250_create_emails_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('emails', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('address')->unique();
+            $table->boolean('is_active')->default(0);
+            $table->boolean('confirmed')->default(0);
+
+            $table->unsignedBigInteger('applicant_id');
+            $table->foreign('applicant_id')
+                ->references('id')
+                ->on('applicants')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('emails');
+    }
+}
