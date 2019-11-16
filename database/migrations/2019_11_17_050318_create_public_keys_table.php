aabc936b428f17d5a8ddf55adfaff17818f4af39
Superern <ernandrewgregorio@gmail.com>
2019-11-17 05:27:40 +0800

    create public_keys migration

---
+++ database/migrations/2019_11_17_050318_create_public_keys_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePublicKeysTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('public_keys', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('key')->unique();
+
+            $table->unsignedBigInteger('applicant_id');
+            $table->foreign('applicant_id')
+                ->references('id')
+                ->on('applicants')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('public_keys');
+    }
+}
