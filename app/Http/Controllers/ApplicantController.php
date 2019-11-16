e0b04deb720f15637bb7c2f1c1b70d8d70335f28
Superern <ernandrewgregorio@gmail.com>
2019-11-17 06:36:47 +0800

    update applicant controller to create record

--- app/Http/Controllers/ApplicantController.php
+++ app/Http/Controllers/ApplicantController.php
@@ -2,9 +2,82 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Address;
+use App\Models\Applicant;
+use App\Models\Country;
+use App\Models\Email;
+use App\Models\Mobile;
+use App\Models\Passport;
+use App\Models\PublicKey;
+use App\Models\Status;
 use Illuminate\Http\Request;
 
 class ApplicantController extends Controller
 {
+    public function store(Request $request) {
+        // validate
+        try {
+            // create applicant with grant type
+            $applicant = Applicant::create([
+                'title' => $request->title,
+                'firstname' => $request->firstname,
+                'lastname' => $request->lastname,
+                'type_id' => $request->type
+            ]);
 
+            // create address
+            Address::create([
+                'street' => $request->address,
+                'city' => $request->city,
+                'state' => $request->state,
+                'zip' => $request->zip,
+                'applicant_id' => $applicant->id
+            ]);
+
+            // create mobile
+            Mobile::create([
+                'number' => $request->mobile,
+                'applicant_id' => $applicant->id
+            ]);
+
+            // create email
+            Email::create([
+                'address' => $request->email,
+                'applicant_id' => $applicant->id
+            ]);
+
+            // create passport
+            Passport::create([
+                'code' => $request->passport,
+                'applicant_id' => $applicant->id
+            ]);
+
+            // create public key
+            PublicKey::create([
+                'key' => $request->publicKey,
+                'applicant_id' => $applicant->id
+            ]);
+
+            // create applicant-country
+            $country = Country::firstOrCreate([
+                'name' => $request->countryName,
+                'code' => $request->countryCode,
+                'symbol' => $request->countrySymbol,
+                'flag' => $request->countryFlag
+            ]);
+            $applicant->originatesIn($country);
+
+            // create applicant-status
+            $status = Status::findOrFail('reviewing');
+            $applicant->attachedStatus($status, $applicant->id, 'Created by System');
+            return response()->json(['status'=> true, 'message'=> 'Successfully sent your application'], 200);
+
+        }catch (\Exception $e) {
+            return response()->json(['status'=>false, 'message'=>'Something went wrong', 'system'=> $e->getMessage()],403);
+        }
+
+
+
+
+    }
 }
