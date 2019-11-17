5e6df936c273f49eed77f4de2944f30d584845cf
Superern <ernandrewgregorio@gmail.com>
2019-11-18 03:32:04 +0800

    create organizations migration

---
+++ database/migrations/2019_11_18_032927_create_organization_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrganizationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('organizations', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name')->unique();
+            $table->string('description')->nullable();
+
+            $table->unsignedBigInteger('applicant_id');
+            $table->foreign('applicant_id')
+                ->references('id')
+                ->on('applicants')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('organizations');
+    }
+}
