122c3716ffd79bac8a599d4f37258b1ab1f7bc95
Superern <ernandrewgregorio@gmail.com>
2019-11-18 03:41:20 +0800

    add foreign key to model fillable

--- app/Models/Organization.php
+++ app/Models/Organization.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Organization extends Model
 {
-    protected $fillable = ['name', 'description'];
+    protected $fillable = ['name', 'description', 'applicant_id'];
 
     public function applicant() {
         return $this->belongsTo(Applicant::class);
