20abcf9c2c9f98b126405dad6a11c0a34003edcf
Superern <ernandrewgregorio@gmail.com>
2019-11-18 09:52:05 +0800

    add google analytics

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,14 @@
 import GrantsIndex from './components/grants/index';
+import VueGtm from 'vue-gtm';
 import './vendors/vue-sweetalert2'
 import './vendors/vee-validate'
 
+Vue.use(VueGtm, {
+    id: 'GTM-WQK7NS3',
+    enabled: true,
+    debug: true,
+    loadScript: true
+});
 new Vue({
     el: '#app',
     components: {
